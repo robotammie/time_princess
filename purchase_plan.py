4b052b9f709794dc9198cdb6a062923e985e4d70
Tammie Kahnhauser <tammie.kahnhauser@gmail.com>
2022-02-03 20:21:40 -0800

    Break data into its own file

--- purchase_plan.py
+++ purchase_plan.py
@@ -1,34 +1,8 @@
 import optimizers
+import spring_prelude
 from classes import Item
 from classes import Plan
 
-money = 1254
-
-items = set({
-    Item('Midspring Plum', 'dress', 700, 6),
-    Item('Midspring Plum', 'hair', 380, 6),
-    Item('Midspring Plum', 'hat', 290,5),
-    Item('Midspring Plum', 'makeup', 90, 5),
-    Item('Midspring Plum', 'earrings', 90, 4),
-    Item('Midspring Plum', 'branch', 400, 6),
-    Item('Midspring Plum', 'gloves', 130, 4),
-    Item('Midspring Plum', 'ring', 80, 4),
-    Item('Midspring Plum', 'stockings', 120, 5),
-    Item('Midspring Plum', 'shoes', 270, 5),
-    Item('', 'fan', 435, 6),
-    Item('Moonlit Cliff', 'dress', 780, 6),
-    Item('Moonlit Cliff', 'hair', 380, 6),
-    Item('Moonlit Cliff', 'hat', 250, 5),
-    Item('Moonlit Cliff', 'makeup', 65, 4),
-    Item('Moonlit Cliff', 'mask', 70, 5),
-    Item('Moonlit Cliff', 'earrings', 110, 5),
-    Item('Moonlit Cliff', 'ring', 80, 4),
-    Item('Moonlit Cliff', 'instrument', 450, 6),
-    Item('Moonlit Cliff', 'stockings', 85, 4),
-    Item('Moonlit Cliff', 'shoes', 270, 5),
-})
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 def purchase_plan(items: [Item],
                   money: int,
@@ -64,5 +38,9 @@ def show_plan(plan: Plan) -> None:
 
 
 if __name__ == '__main__':
-    show_plan(purchase_plan(items, money, optimizers.LeastRemainingMoney()))
+    show_plan(purchase_plan(
+        spring_prelude.items,
+        spring_prelude.money,
+        optimizers.LeastRemainingMoney()
+    ))
 
---
+++ spring_prelude.py
@@ -0,0 +1,28 @@
+from classes import Item
+
+
+money = 2254
+
+items = set({
+    Item('Midspring Plum', 'dress', 700, 6),
+    Item('Midspring Plum', 'hair', 380, 6),
+    Item('Midspring Plum', 'hat', 290,5),
+    Item('Midspring Plum', 'makeup', 90, 5),
+    Item('Midspring Plum', 'earrings', 90, 4),
+    Item('Midspring Plum', 'branch', 400, 6),
+    Item('Midspring Plum', 'gloves', 130, 4),
+    Item('Midspring Plum', 'ring', 80, 4),
+    Item('Midspring Plum', 'stockings', 120, 5),
+    Item('Midspring Plum', 'shoes', 270, 5),
+    Item('', 'fan', 435, 6),
+    Item('Moonlit Cliff', 'dress', 780, 6),
+    Item('Moonlit Cliff', 'hair', 380, 6),
+    Item('Moonlit Cliff', 'hat', 250, 5),
+    Item('Moonlit Cliff', 'makeup', 65, 4),
+    Item('Moonlit Cliff', 'mask', 70, 5),
+    Item('Moonlit Cliff', 'earrings', 110, 5),
+    Item('Moonlit Cliff', 'ring', 80, 4),
+    Item('Moonlit Cliff', 'instrument', 450, 6),
+    Item('Moonlit Cliff', 'stockings', 85, 4),
+    Item('Moonlit Cliff', 'shoes', 270, 5),
+})
